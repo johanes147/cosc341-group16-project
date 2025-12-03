2408d36f28c91abeca03e8274d6d5d71e883512e
Benedict Astan <sergioastan@gmail.com>
2025-12-02 18:20:55 -0800

    Created basic layout for profile page

---
+++ app/src/main/java/com/example/myapplication/ProfilePage.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        BottomNavigationView bnv = findViewById(R.id.bottom_navigation);
+
+        // Set the "Home" item as the selected one since we are on the Home screen
+        bnv.setSelectedItemId(R.id.nav_home);
+
+        bnv.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                // We are already on the home screen, so do nothing.
+                return true;
+            } else if (itemId == R.id.nav_fav) {
+                startActivity(new Intent(getApplicationContext(), FavoriteActivity.class));
+                overridePendingTransition(0, 0); // Optional: removes the default animation
+                finish(); // Close HomeActivity to prevent a stack of activities
+                return true;
+            } else if (itemId == R.id.nav_sell) {
+                startActivity(new Intent(getApplicationContext(), SellActivity.class));
+                // Don't finish HomeActivity, so the user can easily return
+                return true;
+            }  else if (itemId == R.id.nav_chat) {
+                startActivity(new Intent(getApplicationContext(), ChatListActivity.class));
+                return true;
+            }
+
+            // Add other navigation items (chat, profile) here in the future
+            return false;
+        });
+    }
+}
